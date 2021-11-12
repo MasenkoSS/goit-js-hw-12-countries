4a664faf03d43a370e2e1416c9f4602887d60bdd
Sergii <83786081+MasenkoSS@users.noreply.github.com>
2021-11-12 14:03:33 +0300

    Update final vers

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,10 @@
+const BASE_URL = 'https://restcountries.com/v2';
+
+function fetchOneCountry(name) {
+  const url = `${BASE_URL}/name/${name}`;
+  return fetch(url).then(response => {
+    return response.json();
+  });
+}
+
+export default { fetchOneCountry };
