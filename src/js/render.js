f43e4fd94879e13d5fcd07cac7379b8cd26338ae
Sergii <83786081+MasenkoSS@users.noreply.github.com>
2021-11-15 16:16:19 +0300

    fix ment

--- src/js/render.js
+++ src/js/render.js
@@ -10,49 +10,51 @@ import API from '../js/fetchCountries.js';
 
 const refs = getRefs();
 
-refs.searchField.addEventListener('input', debounce(onSearch, 2500));
+refs.searchField.addEventListener('input', debounce(onSearch, 1000));
+
+
 
 function onSearch(event) {
-  console.log(event.target.value);
-  const searchKey = event.target.value;
-  API.fetchOneCountry(searchKey).then(render);
-  };
+    console.log(event.target.value);
+    const searchKey = event.target.value;
+    API.fetchOneCountry(searchKey).then(render);
+}
 
 function renderCountryCard(country) {
-  const markup = countryCard(country);
-  console.log(markup);
-  
-  refs.cardContainer.innerHTML = markup;
+    const markup = countryCard(country);
+    console.log(markup);
+
+    refs.cardContainer.innerHTML = markup;
 }
 
 function renderCountryList(value) {
-  const markupList = countryList(value);
-  console.log(markupList);
-  refs.cardContainer.innerHTML = markupList;
+    const markupList = countryList(value);
+    console.log(markupList);
+    refs.cardContainer.innerHTML = markupList;
 }
 
 function render(value) {
-  console.log(value.length);
-  if (value.length === 1) {
-    renderCountryCard(value);
-  } else if (value.length > 1 && value.length < 11) {
-    renderCountryList(value);
-  } else if (!value.length) {
-    refs.cardContainer.innerHTML = '';
-    onError(`Будь ласка спробуйте ще раз.`)
-  } else {
-    onError(`Будь ласка, введіть більш конкретний запит.`)
-  }
+    console.log(value.length);
+    if (value.length === 1) {
+        renderCountryCard(value);
+    } else if (value.length > 1 && value.length < 11) {
+        renderCountryList(value);
+    } else if (!value.length) {
+        refs.cardContainer.innerHTML = '';
+        onError(`Будь ласка спробуйте ще раз.`);
+    } else {
+        onError(`Будь ласка, введіть більш конкретний запит.`);
+    }
 }
 
 export function onError(message) {
-  return error({
-    text: message,
-    delay: 2000,
-    closer: false,
-    title: '',
-    icon: false,
-    width: '250px',
-    sticker: false,
-     })
+    return error({
+        text: message,
+        delay: 2000,
+        closer: false,
+        title: '',
+        icon: false,
+        width: '250px',
+        sticker: false,
+    });
 }
